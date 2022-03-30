0d0a0982cce9622d46404bce72536ae969d87975
JENNIFER Chan <jdchan@jen-chan.myfiosgateway.com>
2022-03-29 21:34:41 -0400

    first commit

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+
+class Config():
+    """
+    Set configuration variables for the app using env variables where
+    available and creating config variables when not
+    """
+    FLASK_APP = os.getenv('FLASK_APP')
+    FLASK_ENV = os.getenv('FLASK_ENV')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'This is a secret key'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_NOTIFICATIONS = False 
+    
